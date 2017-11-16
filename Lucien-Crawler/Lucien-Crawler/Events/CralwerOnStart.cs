49102ab7d3f2a0bdccc979be348157b78547a83d
NigulasiZhao <185653517@qq.com>
2017-11-16 21:31:06 +0800

    爬虫相关事件

---
+++ Lucien-Crawler/Lucien-Crawler/Events/CralwerOnStart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucien_Crawler.Events
+{
+    public class CralwerOnStart
+    {
+        public Uri Url { get; set; }// 爬虫URL地址
+
+        public CralwerOnStart(Uri url)
+        {
+            this.Url = url;
+        }
+    }
+}
---
+++ Lucien-Crawler/Lucien-Crawler/Events/CrawlerOnError.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucien_Crawler.Events
+{
+    public class CrawlerOnError
+    {
+        public Uri Url { get; set; }
+
+        public Exception Exception { get; set; }
+
+        public CrawlerOnError(Uri url, Exception exception)
+        {
+            this.Url = url;
+            this.Exception = exception;
+        }
+    }
+}
