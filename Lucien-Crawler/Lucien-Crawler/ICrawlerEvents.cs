9c77023c90265df22b2a53b2e0e0c5350304a003
NigulasiZhao <185653517@qq.com>
2017-11-16 21:41:48 +0800

    接口相关

---
+++ Lucien-Crawler/Lucien-Crawler/ICrawlerEvents.cs
@@ -0,0 +1,20 @@
+﻿using Lucien_Crawler.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucien_Crawler
+{
+    public interface ICrawlerEvents
+    {
+        event EventHandler<CralwerOnStart> OnStart;//爬虫启动事件
+
+        event EventHandler<CrawlerOnComplete> OnCompleted;//爬虫完成事件
+
+        event EventHandler<CrawlerOnError> OnError;//爬虫出错事件
+
+        Task<string> Start(Uri uri, string proxy); //异步爬虫
+    }
+}
