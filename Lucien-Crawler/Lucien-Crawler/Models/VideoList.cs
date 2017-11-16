e6c017d8928821c90afe637a3cd951469c2cf731
NigulasiZhao <185653517@qq.com>
2017-11-16 21:38:42 +0800

    添加实体

---
+++ Lucien-Crawler/Lucien-Crawler/Models/VideoList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucien_Crawler.Models
+{
+    public class VideoList
+    {
+
+        public string RegionName { get; set; }
+
+        public Uri Uri { get; set; }
+    }
+}
